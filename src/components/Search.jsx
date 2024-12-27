a94494bef49013fa2b8e2732d4bbaf970a7bc5fb
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-12-27 12:46:53 -0500

    Completed Search and added styling

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,18 +1,16 @@
 import React, { useState } from 'react'
-
+import styles from './Search.module.css'
 
 export default function Search({ onSearch }) {
-
 
     function handleChange(event) {
         onSearch(event.target.value)
     }
 
-
     return (
-        <>
-            <label>Search Pokémon: </label>
-            <input type="text" placeholder='Pikachu' onChange={handleChange}></input>
-        </>
+        <div className={styles.searchContainer}>
+            <label className={styles.searchLabel}>Search Pokémon: </label>
+            <input className={styles.searchInput} type="text" placeholder='Pikachu' onChange={handleChange}></input>
+        </div>
     )
 }
